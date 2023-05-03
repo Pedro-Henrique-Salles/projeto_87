5e2e5d68e3b0abe1530b6bf1d26eb5a2a25ac29d
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-05-03 15:01:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+
+// Create canvas variable
+var canvas = new fabric.Canvas('myCanvas');
+
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function new_image(getImage)
+{
+	// para enviar imagens
+	fabric.Image.fromURL(get_image, function(Img) {
+		block_image_object = Img;
+
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set({
+		top	:block_y,
+		left:block_x
+		});
+		canvas.add(block_image_object);
+	});
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '69') // adicione os códigos adequados às teclas
+	{
+		// enviar ranger escarlate
+		new_image('rr1.png');
+		console.log("e");
+	}
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		// enviar ranger verde
+		new_image('gr.png');
+		console.log("v");
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		// enviar ranger amarelo
+		new_image('yr.png');
+		console.log("a");
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		new_image('pr.png');
+		console.log("r");
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+	// enviar ranger índigo
+		new_image('br.png')
+		console.log("i");
+	}
+	
+}
+
